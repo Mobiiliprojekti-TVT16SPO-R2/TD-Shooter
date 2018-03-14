77c583c078326d35f1328e92e05aaf05e9671242
Leevi Mustajärvi <leevi.mustajarvi@gmail.com>
2018-03-14 09:43:06 +0200

    lisätty toinen kuva näytölle

--- core/src/tdshooter/game/TDShooterGdxGame.java
+++ core/src/tdshooter/game/TDShooterGdxGame.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class TDShooterGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture testi;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		testi = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -22,6 +24,7 @@ public class TDShooterGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(testi, 400, 400);
 		batch.end();
 	}
 	
