0cfb3468e0865963ce634f57057d97a06bb84501
Leevi Mustajärvi <leevi.mustajarvi@gmail.com>
2018-03-14 09:31:02 +0200

    Testataan projektin luontia ja committausta

---
+++ android/src/tdshooter/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package tdshooter.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tdshooter.game.TDShooterGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TDShooterGdxGame(), config);
+	}
+}
