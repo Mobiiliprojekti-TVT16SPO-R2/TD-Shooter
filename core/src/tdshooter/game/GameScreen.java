f65b803633147825621ace7e3a9b05e2ff35ff51
Leeway91 <32339125+leeway91@users.noreply.github.com>
2018-03-19 12:01:26 +0200

    Törmäyksien tarkistus for-loopeilla.

--- core/src/tdshooter/game/GameScreen.java
+++ core/src/tdshooter/game/GameScreen.java
@@ -1,5 +1,6 @@
 package tdshooter.game;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -27,8 +28,8 @@ public class GameScreen implements Screen {
     Sound dropSound;
     Music rainMusic;
     OrthographicCamera camera;
-    Array<Encounter> raindrops;
-    Array<Projectile> playerProjectiles;
+    ArrayList<Encounter> raindrops;
+    ArrayList<Projectile> playerProjectiles;
 
     long lastDropTime;
     long lastBulletTime;
@@ -60,12 +61,12 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, viewPortWidth, viewPortHeight);
 
-        // create the raindrops array and spawn the first raindrop
-        raindrops = new Array<Encounter>();
+        // create the raindrops arraylist and spawn the first raindrop
+        raindrops = new ArrayList<Encounter>();
         spawnRaindrop();
 
-        //create playerprojectilearray
-        playerProjectiles = new Array<Projectile>();
+        //create playerprojectilearraylist
+        playerProjectiles = new ArrayList<Projectile>();
 
     }
 
@@ -112,7 +113,7 @@ public class GameScreen implements Screen {
             player.hitbox.x = touchPos.x - 64 / 2;
 
             // check if we need to create a new bullet
-            if (TimeUtils.nanoTime() - lastBulletTime > 200000000)
+            if (TimeUtils.nanoTime() - lastBulletTime > 100000000)
                 spawnBullet();
         }
 //        if (Gdx.input.isKeyPressed(Keys.LEFT))
@@ -121,7 +122,7 @@ public class GameScreen implements Screen {
 //            bucket.x += 200 * Gdx.graphics.getDeltaTime();
 
         // check if we need to create a new raindrop
-        if (TimeUtils.nanoTime() - lastDropTime > 400000000)
+        if (TimeUtils.nanoTime() - lastDropTime > 200000000)
             spawnRaindrop();
         
         MoveAllObjects();
@@ -131,31 +132,29 @@ public class GameScreen implements Screen {
         // move the raindrops, remove any that are beneath the bottom edge of
         // the screen or that hit the bucket. In the later case we increase the
         // value our drops counter and add a sound effect.
-        Iterator<Encounter> iter = raindrops.iterator();
-        Iterator<Projectile> playerBulletIter = playerProjectiles.iterator();
-        while (iter.hasNext()) {
-            Encounter raindrop = iter.next();
-            if (raindrop.hitbox.y + 64 < 0)
-                iter.remove();
-            if (raindrop.overlaps(player.hitbox)){
+
+        for (int i = 0; i < raindrops.size(); i++) {
+            Encounter raindrop = raindrops.get(i);
+            if (raindrop.hitbox.y + 64 < 0) {
+                raindrops.remove(i);
+            } else if (raindrop.overlaps(player.hitbox)){
                 dropsGathered++;
                 dropSound.play();
                 raindrop.collidesWith(player);
                 player.collidesWith(raindrop);
             }
-            while (playerBulletIter.hasNext()) {
-                Projectile bullet = playerBulletIter.next();
+            for (int j = 0; j < playerProjectiles.size(); j++) {
+                Projectile bullet = playerProjectiles.get(j);
                 if (bullet.hitbox.y > viewPortHeight - 100) {
-                    playerBulletIter.remove();
+                    playerProjectiles.remove(j);
                 } else if (bullet.overlaps(raindrop.hitbox)){
                     dropSound.play();
                     raindrop.getsDamage(bullet.damage);
-                    playerBulletIter.remove();
-
+                    playerProjectiles.remove(j);
                 }
             }
             if (raindrop.isDestroyed()){
-                iter.remove();
+                raindrops.remove(i);
             }
         }
 
@@ -170,8 +169,8 @@ public class GameScreen implements Screen {
         game.font.draw(game.batch, "FPS: " + fps, 0, viewPortHeight - 30);
         game.font.draw(game.batch, "Drops Collected: " + dropsGathered, 0, viewPortHeight);
         game.font.draw(game.batch, "Player HP: " + player.getHitPoints(), 0 , viewPortHeight - 60);
-        game.font.draw(game.batch, "Projectiles: " + playerProjectiles.size, 0 , viewPortHeight - 90);
-        game.font.draw(game.batch, "Raindrops: " + raindrops.size, 0 , viewPortHeight - 120);
+        game.font.draw(game.batch, "Projectiles: " + playerProjectiles.size(), 0 , viewPortHeight - 90);
+        game.font.draw(game.batch, "Raindrops: " + raindrops.size(), 0 , viewPortHeight - 120);
         game.batch.draw(player.playerImage, player.hitbox.getX(), player.hitbox.getY(), player.hitbox.getWidth(), player.hitbox.getHeight());
         for (Encounter raindrop : raindrops) {
             game.batch.draw(raindrop.encounterImage, raindrop.hitbox.x, raindrop.hitbox.y);
