9b98599479f55242d9dff51d4da0d2ecb74de52e
Samuli Lehto <t6lela00@students.oamk.fi>
2018-04-13 09:12:59 +0300

    Mission-luokka lukee lentoratoja tiedostosta.

--- core/src/tdshooter/game/EncounterBuilder.java
+++ core/src/tdshooter/game/EncounterBuilder.java
@@ -42,26 +42,22 @@ public class EncounterBuilder
         {
             case BASIC_ENEMY_LVL1:
                 encounter = new Encounter(1000, 2000, 64, 96,20, 20,400 , 25, (Texture)assets.get("Encounters/AlienBeast_LVL_1_Test.png"));
-                encounter.setFlightPattern(new SinCurveFlight(encounter, 20, MathUtils.PI2));
                 break;
 
             case SHOOTING_ENEMY_LVL1:
                 encounter = new ShootingEnemy(1000, 2000, 64, 112,
                         20, 20,300 , 1, 0, 500000000, 40,
                         50 , (Texture)assets.get("Encounters/AlienFighter_LVL_1_Test.png"), assets);
-                encounter.setFlightPattern(new SinCurveFlight(encounter, 20, MathUtils.PI2));
                 break;
 
             case SHOOTING_ENEMY_LVL2:
                 encounter = new ShootingEnemy(1000, 2000, 96, 128,
                         150, 40,120 , 3, 200, 2100000000, 300,
                         200 , (Texture)assets.get("Encounters/AlienFighter_LVL_2_VERSION2.png"), assets);
-                encounter.setFlightPattern(new StraightFlight(encounter,30));
                 break;
 
             default:
                 encounter = new Encounter(1000, 2000, 64, 64,20, 20,100 , 25, (Texture)assets.get("Encounters/AlienBeast_LVL_1_Test.png"));
-                encounter.setFlightPattern(new StraightFlight(encounter, 0));
                 break;
         }
         return encounter;
