682922170374aaa1f4ce871f76c9d81e69cec317
Leeway91 <32339125+leeway91@users.noreply.github.com>
2018-04-19 09:28:19 +0300

    updated first mission

--- core/src/tdshooter/game/EncounterBuilder.java
+++ core/src/tdshooter/game/EncounterBuilder.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.math.MathUtils;
  */
 enum EncounterType
 {
-    BASIC_ENEMY_LVL1(0), SHOOTING_ENEMY_LVL1(1), SHOOTING_ENEMY_LVL2(2), SHOOTING_ENEMY_LVL3(3), BASIC_ENEMY_LVL3(3);
+    BASIC_ENEMY_LVL1(0), SHOOTING_ENEMY_LVL1(1), SHOOTING_ENEMY_LVL2(2), SHOOTING_ENEMY_LVL3(3), BASIC_ENEMY_LVL2(4), BASIC_ENEMY_LVL3(5), FIGHTERBRUISER_ENEMY(6), ORGANIC_MINE(7), EXPLOSIVE_MINE(7);
 
     private int value;
 
@@ -41,7 +41,9 @@ public class EncounterBuilder
         switch(enemyType)
         {
             case BASIC_ENEMY_LVL1:
-                encounter = new Encounter(1000, 2000, 64, 96,20, 20,400 , 25, (Texture)assets.get("Encounters/AlienBeast_LVL_1_Test.png"));
+                encounter = new Encounter(1000, 2000, 64, 96,
+                        20, 20,400 , 25,
+                        (Texture)assets.get("Encounters/AlienBeast_LVL_1_Test.png"));
                 break;
 
             case SHOOTING_ENEMY_LVL1:
@@ -52,10 +54,33 @@ public class EncounterBuilder
 
             case SHOOTING_ENEMY_LVL2:
                 encounter = new ShootingEnemy(1000, 2000, 96, 128,
-                        150, 40,120 , 3, 200, 2100000000, 300,
-                        200 , (Texture)assets.get("Encounters/AlienFighter_LVL_2_VERSION2.png"), assets);
+                        50, 40,180 , 2, 200, 2100000000, 300,
+                        100 , (Texture)assets.get("Encounters/AlienFighter_LVL_2_VERSION2.png"), assets);
                 break;
 
+            case SHOOTING_ENEMY_LVL3:
+                encounter = new ShootingEnemy(1000, 2000, 128, 128,
+                        100, 60,160 , 3, 200, 2100000000, 300,
+                        200 , (Texture)assets.get("Encounters/AlienFighter_LVL_3_VERSION2.png"), assets);
+                break;
+
+            case BASIC_ENEMY_LVL2:
+                encounter = new Encounter(1000, 2000, 96, 96,
+                        40, 30,350 , 50,
+                        (Texture)assets.get("Encounters/AlienBeast_LVL_2.png"));
+                break;
+
+            case BASIC_ENEMY_LVL3:
+                encounter = new Encounter(1000, 2000, 128, 128,
+                        60, 50,300 , 150,
+                        (Texture)assets.get("Encounters/AlienBeast_LVL_3.png"));
+                break;
+
+            case FIGHTERBRUISER_ENEMY:
+                encounter = new ShootingEnemy(1000, 2000, 140, 140,
+                        200, 60,160 , 5, 300, 1500000000, 280,
+                        400 , (Texture)assets.get("Encounters/AlienFighter_Bruiser.png"), assets);
+                break;
             default:
                 encounter = new Encounter(1000, 2000, 64, 64,20, 20,100 , 25, (Texture)assets.get("Encounters/AlienBeast_LVL_1_Test.png"));
                 break;
